d8b89c90ce8879ea934f5e0e94e27ca7280d1024
ameda-kevin <amedakevin@gmail.com>
2024-07-30 22:18:46 +0300

    hamcrest assertions and matchers

--- src/test/java/com/ameda/kevin/service/UserServiceTest.java
+++ src/test/java/com/ameda/kevin/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.ameda.kevin.service;
 
 import com.ameda.kevin.entity.User;
 import com.ameda.kevin.repository.UserRepository;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,14 +27,14 @@ public class UserServiceTest {
 
     @Test
     public void getRegularUsers() {
-        /*arrange*/
+        /* arrange */
         List<User> users = new ArrayList<>();
         users.add(User.createRegularUser("kevin","ameda"));
         users.add(User.createRegularUser("oscar","kisala"));
         users.add(User.createRegularUser("judith","angiya"));
         users.add(User.createRegularUser("ruth","andisi"));
         Mockito.when(repository.findAll()).thenReturn(users);
-        /*act*/
+        /* act */
         Set<User> actualUsers  = userService.getRegularUsers();
         /* assert */
 
@@ -45,6 +46,30 @@ public class UserServiceTest {
             Assert.assertEquals(actualUser.getUserType(), User.UserType.REGULAR_USER);
         }
     }
+
+    @Test
+    public void getRegularUsers_hamcrest() {
+        /* arrange */
+        List<User> users = new ArrayList<>();
+        users.add(User.createRegularUser("kevin","ameda"));
+        users.add(User.createRegularUser("oscar","kisala"));
+        users.add(User.createRegularUser("judith","angiya"));
+        users.add(User.createRegularUser("ruth","andisi"));
+        Mockito.when(repository.findAll()).thenReturn(users);
+        /* act */
+        Set<User> actualUsers  = userService.getRegularUsers();
+        /* assert */
+
+        //Hamcrest descriptive assertions
+        Assert.assertThat(actualUsers, CoreMatchers.is(CoreMatchers.notNullValue()));
+        Assert.assertThat(actualUsers.size(), CoreMatchers.equalTo(4));
+        for(User actualUser: actualUsers){
+            Assert.assertThat(actualUsers, CoreMatchers.is(CoreMatchers.notNullValue()));
+            Assert.assertNotNull(actualUser); // at least the object should not be null.
+            Assert.assertEquals(actualUser.getUserType(), User.UserType.REGULAR_USER);
+        }
+    }
+
 
     @Test
     public void getAdminUsers() {
