7be0420f3e49e22b194b72e96bed6bc846a2b726
ameda-kevin <amedakevin@gmail.com>
2024-07-30 03:43:30 +0300

    working with the AAA framework

--- src/test/java/com/ameda/kevin/CalculatorTest.java
+++ src/test/java/com/ameda/kevin/CalculatorTest.java
@@ -1,21 +1,28 @@
 package com.ameda.kevin;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 
 /*
 @author ameda
 */
+
 public class CalculatorTest{
 
     //Arrange (setup)
     // Act (SUT)
     // Assert (few assertions) framework
+
     @Test
     public void testAdd() {
-
+        //Arrange
+        int a=100, b =50;
+        //Act
+        int result = Calculator.add(a,b);
+        //Assert
+        assertEquals(150,result);
     }
-
 
 }
